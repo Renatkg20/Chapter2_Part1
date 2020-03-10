d7338e1587ce14215dc246fae35c73586f854f9f
[Renatkg20] <renatkg2005@gmail.com>
2020-03-10 10:11:37 +0600

    The task 1 of Part 1 in the Chapter 2 updated

--- task1_1.py
+++ task1_1.py
@@ -2,4 +2,6 @@ import re
 g = input ("Input the sentence ")
 result = re.findall("[A-Z]", g)
 final_result = ((len(result) *100) / len(g))
+final_result1 = 100 - final_result
 print(f"{round(final_result, 2)} % is capital letters in the sentence.")
+print(f"{round(final_result1, 2)} % is lower letters in the sentence.")
