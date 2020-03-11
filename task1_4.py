a35d9e73da3ebf76235b0167269519fb6fc48816
[Renatkg20] <renatkg2005@gmail.com>
2020-03-11 12:33:23 +0600

    Update task

--- task1_4.py
+++ task1_4.py
@@ -1,16 +1,20 @@
 list_ = (input('Pls the list for coding ')).split()
 new_list = [int(i) for i in list_]
 result = max(new_list) - 1
+
 if max(new_list) < 0:
 	result = abs(max(new_list))
+elif result in new_list:
+	result = result + 2
 
 print(result)
 
-def max1(new_list):
-	result = max(new_list) - 1
-	if max(new_list) < 0:
-		result = abs(max(new_list))
+# def max1(new_list):
+# 	result = max(new_list) - 1
 
-	return (result)
+# 	elif max(new_list) < 0:
+# 		result = abs(max(new_list))
 
-print(max1(new_list))
+# 	return (result)
+
+# print(max1(new_list))
